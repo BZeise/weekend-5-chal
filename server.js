39f203a5d584318e4c6b2a26067def4197a7a3b4
Ben Zeise <ben.zeise@gmail.com>
2017-06-16 16:49:33 -0500

    barebones, server up and listening on 7878

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require( 'express' );
+var app = express();
+var path = require( 'path' );
+var bodyParser = require( 'body-parser' );
+var index = require( './modules/routes/index' );
+//var listing = require( './modules/routes/listing' );
+
+app.use( '/', index );
+//app.use( '/listing', listing );
+app.use( bodyParser.json() );
+app.use( express.static( 'public' ) );
+
+var port = process.env.PORT || 7878;
+
+app.listen( port, function( ){
+  console.log( 'listening on', port );
+});
